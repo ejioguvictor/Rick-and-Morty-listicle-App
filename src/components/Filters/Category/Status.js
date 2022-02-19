3421ae117e9dbcd4341b83266cf481e696b5cf56
ejioguvictor <ejioguvictor6054@gmail.com>
2022-02-19 07:13:37 +0100

    updating files

--- src/components/Filters/Category/Status.js
+++ src/components/Filters/Category/Status.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import FilterBTN from '../FilterBTN';
 
 const Status = () => {
+
+  let status = ["Alive", "Dead", "Unknown"]
   return (
     <div className="accordion-item">
       <h2 className="accordion-header" id="headingThree">
@@ -10,6 +13,9 @@ const Status = () => {
       </h2>
       <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
         <div className="accordion-body">
+          {status.map((items, index) => (
+            <FilterBTN />
+          ))}
         </div>
       </div>
     </div>
--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -3,7 +3,7 @@ import Gender from "./Category/Gender";
 import Species from "./Category/Species";
 import Status from "./Category/Status"
 
-const filters = () => {
+const Filters = () => {
   return <div className='col-3'>
     <div className="text-center fw-bold fs-4 mb-2">Filter</div>
     <div style={{ cursor: 'pointer' }} className="text-center text-primary text-decoration-underline mb-4" > Clear Filters</div>
@@ -17,4 +17,4 @@ const filters = () => {
   </div >;
 };
 
-export default filters;
+export default Filters;
