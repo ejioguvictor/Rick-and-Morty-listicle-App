36b18e7602d24d60d29b6cfaffebd41fda92c19d
ejioguvictor <ejioguvictor6054@gmail.com>
2022-02-19 07:21:40 +0100

    updating files

--- src/components/Filters/Category/Status.js
+++ src/components/Filters/Category/Status.js
@@ -14,7 +14,7 @@ const Status = () => {
       <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
         <div className="accordion-body">
           {status.map((items, index) => (
-            <FilterBTN />
+            <FilterBTN key={index} />
           ))}
         </div>
       </div>
